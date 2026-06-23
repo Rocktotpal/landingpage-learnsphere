8e0d13e932dc7bd2853cbb33b0952e8a991c4206
Rocktotpal Mahanta <mahantarocktotpal@gmail.com>
2026-06-23 07:03:20 +0600

    added css & js for overlay

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const overlay = document.querySelector(".overlay");
 for (let i = 0; i < btnShowModal.length; i++) {
   btnShowModal[i].addEventListener("click", function () {
     modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
 }
 
 btnCloseModal.addEventListener("click", function () {
   modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
